f8064874c96bb9876155db3fabcdd8c48f1332b0
Abdelrahman Abdelwahab <abdelrahman.abdelwahab@mooney.it>
2023-03-25 02:06:57 +0100

    v 0.0.1

---
+++ HCM/Pods/Target Support Files/Pods-AthanKeeper/Pods-AthanKeeper-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AthanKeeper : NSObject
+@end
+@implementation PodsDummy_Pods_AthanKeeper
+@end
--- HCM/Pods/Target Support Files/Pods-AthanKeeper/Pods-AthanKeeper-umbrella.h
+++ HCM/Pods/Target Support Files/Pods-AthanKeeper/Pods-AthanKeeper-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_AgainVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_AgainVersionString[];
+FOUNDATION_EXPORT double Pods_AthanKeeperVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_AthanKeeperVersionString[];
 
